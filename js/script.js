fe38f5c82084f5d3d890b790623b14816aa6ea5f
Masako16r <brunozeb.pr@gmail.com>
2025-02-05 11:43:23 +0100

    Version 1.7

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// Función para alternar el menú
-function toggleMenu() {
-    const menu = document.getElementById('menuDropdown');
-    if (menu) {
-        menu.classList.toggle('active');
-    } else {
-        console.warn('MenuDropdown no encontrado');
-    }
-}
